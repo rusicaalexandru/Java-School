1d8801ad00144fb901d36773347ba90bbefab620
Rusica Alexantru <aalexandruslan@gmail.com>
2021-03-30 12:00:57 +0300

    Backup

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ public class Main {
         }
         Outer out = new Outer(tablou);
         out.display();
-        Outer.Inner iner = out.new Inner();
     }
 }
 class Outer{
@@ -27,18 +26,30 @@ class Outer{
     }
     class Inner{
         void min(){
+            int min = 100000;
             for(int i = 0; i< array.length; i++){
-
+                if(min > array[i]) min = array[i];
             }
+            System.out.println("Min : " + min);
         }
         void average(){
+            int acumulator = 0;
             for(int i = 0; i< array.length; i++){
-
+                acumulator += array[i];
             }
+            int media = acumulator/array.length;
+            System.out.println("Media : " + media);
         }
         void sumEven(){
             for(int i = 0; i< array.length; i++){
-
+                if(i % 2 == 1){
+                    if(i < array.length-2){
+                        array[i] *= array[i+2];
+                    }
+                }
+            }
+            for(int i = 0; i< array.length; i++){
+                System.out.println(array[i]);
             }
         }
     }
