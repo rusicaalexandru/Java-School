4419ccd8e8e4140f98d3099358e37f9b92513dfa
Rusica Alexantru <aalexandruslan@gmail.com>
2021-03-30 11:26:48 +0300

    Backup

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 public class Main {
     public static void main(String args[]) {
-        
+        System.out.println("Cite cifre va contine tabloul");
+        Scanner in = new Scanner(System.in);
+        int tablou[] = new int[in.nextInt()];
+        System.out.println("Introduceti cifrele : ");
+        for(int i = 0; i < tablou.length; i++){
+            tablou[i] = in.nextInt();
+        }
     }
 }
