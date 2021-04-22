b87c76d4a5323ff5207ed851fda8cd408e3f289a
Rusica Alexantru <aalexandruslan@gmail.com>
2021-04-22 18:18:06 +0300

    finish

--- src/Main5.java
+++ src/Main5.java
@@ -5,24 +5,40 @@ import java.util.concurrent.Flow;
 import javax.swing.*;
 
 class FereastraL7Calculator extends JFrame implements ActionListener{
+    JLabel answer;
+    JTextField input1, input2;
     public FereastraL7Calculator(String name) {
         super(name);
-        setSize(400, 500);
+        setSize(500, 100);
         setLocation(300, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         Container x = getContentPane();
         x.setLayout(new FlowLayout());
 
+        input1 = new JTextField(10);
+        x.add(input1);
+        JLabel operand = new JLabel("+");
+        x.add(operand);
+        input2 = new JTextField(10);
+        x.add(input2);
+
+
         JButton calculateB = new JButton("Calculate");
         x.add(calculateB);
         calculateB.addActionListener(this);
-        JLabel answerL = new JLabel("Hello");
-        x.add(answerL);
+        answer = new JLabel("Hello");
+        x.add(answer);
+
     }
     public void actionPerformed(ActionEvent ae) {
-        if(ae.getActionCommand().compareTo("yellow") == 0){ //delete and replace with other code...
-        }
+        String in1 = input1.getText();
+        String in2 = input2.getText();
+        int a = Integer.parseInt(in1);
+        int b = Integer.parseInt(in2);
+        int c = a + b;
+        String result = String.valueOf(c);
+        answer.setText(result);
     }
 }
 
